544f1d6a85fc6d5cfb5f42a47a9050a238e24065
V3Solution <mubeenyasin75@gmail.com>
2024-05-21 14:22:06 +0500

    complete navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./navbar.scss";
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="real eastate" />
+          <span>Propware</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/" className="register">
+          Sign up
+        </a>
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt="real eastate"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sing up</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/routes/homePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import './homePage.scss'
+
+function HomePage(){
+  return (
+      <div className='homePage'>
+          <div className="textContainer">Text Container</div>
+          <div className="imageContainer">
+          <img src="/bg.png" alt="propware real estate" />
+          </div>
+      </div>
+  )
+}
+
+export default HomePage
